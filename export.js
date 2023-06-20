cd99650dd2a2e332fb2cf45e52eca028c1d143ae
1AndOne1 <112853915+1AndOne1@users.noreply.github.com>
2023-06-20 23:10:35 +0500

    Add files via upload

--- export.js
+++ export.js
@@ -16,6 +16,7 @@ form.addEventListener('submit', async (event) => {
       headers: {
         'Content-Type': 'application/json'
       },
+      mode: "no-cors",
       body: JSON.stringify({ nickname, email, password })
     });
 
